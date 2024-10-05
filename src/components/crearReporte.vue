6df72e48bd9da0627dc6c23216ac9da170548c56
maria <94370096+mariarodriguez217@users.noreply.github.com>
2024-10-05 19:31:19 -0300

    actualizacion crear reporte filas

--- src/components/crearReporte.vue
+++ src/components/crearReporte.vue
@@ -1,12 +1,63 @@
 <template>
-  <h1>reporte de proceso de tareas</h1>
-  <h1>detalle de tareas</h1>
-  </template>
-  
-  <script>
+<table border="3">
+  <thead>
+    <tr>
+      <th>Reportes</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr onclick="filaSeleccionada(1)">
+      <td>Proceso de tareas</td>
+    </tr>
+    <tr onclick="filaSeleccionada(2)">
+      <td>Detalles de tarea</td>
+    </tr>
+    <tr onclick="filaSeleccionada(3)">
+      <td>Todas las tareas</td>
+    </tr>
+    <tr onclick="filaSeleccionada(4)">
+      <td>Nuevo reporte</td>
+    </tr>
+  </tbody>
+</table>
 
-  </script>
+</template>
+
+
+
+ 
+<script>
+  function filaSeleccionada(fila) {
+    alert("Has seleccionado la fila " + fila);
+    // Aquí puedes ejecutar alguna acción como redirigir a otra página.
+  }
+</script>
+  
   
   <style scoped>
-  /* Personaliza los estilos si es necesario */
+ table {
+      width: 90%;
+      border-collapse: collapse;
+    }
+    th{
+      text-align: center;
+      font-size: 30px;
+    }
+     td {
+      border: 1px solid black;
+      padding: 10px;
+      text-align:left;
+      font-family:'Lucida Sans', 'Lucida Sans Regular';
+      font-size: 20px;
+    }
+    tr {
+      height: 80px; /* Altura de cada fila */
+    }
+    tbody tr {
+      cursor: pointer;
+      transition: background-color 0.3s ease;
+    }
+    tbody tr:hover {
+      background-color: #f0f0f0;
+    }
   </style>
